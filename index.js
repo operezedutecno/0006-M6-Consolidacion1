b15fdc4d52a4b1ab47df812fdc951090e03cc478
Jedicillo <seron.cesar@gmail.com>
2022-03-26 13:08:57 -0300

    Agrega busqueda por tipo de empleado

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.listen(3000, () => console.log("Servidor activo http://localhost:3000"));
 
 app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`));
 app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`));
+
+app.get("/registro", (req, res) => {
+
+});
+
+app.get("/listado", (req, res) => {
+    fs.readFile(`${__dirname}/data/data.json`, "utf8", (error, contenido) => {
+        let {tipoEmpleados, empleados} = JSON.parse(contenido);
+        let desc = empleados.map(empleado => {
+            empleado.tipoEmpleado = tipoEmpleados.find(tipoEmpleado => {
+                return tipoEmpleado.id == empleado.tipoEmpleadoId
+            })
+            return empleado;
+        })
+        res.setHeader("content-type", "application/json");
+        res.json(desc);
+    });
+});
+
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
