3e74ff719235ae44a45a245a4c958920bda2db61
Jez <jezielandres@gmail.com>
2022-03-26 12:26:20 -0300

    menu avanzado

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => console.log("Servidor activo http://localhost:3000"))
+app.listen(3000, () => console.log("Servidor activo http://localhost:3000"));
 
-app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`))
-app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`))
+app.use("/bootstrap", express.static(`${__dirname}/node_modules/bootstrap/dist/`));
+app.use("/jquery", express.static(`${__dirname}/node_modules/jquery/dist/`));
 
 app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/views/index.html`);
-})
+});
